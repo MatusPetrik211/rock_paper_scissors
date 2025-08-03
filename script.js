c629a68d9cd52ed0291eb41ca36fd5abb4fe50d1
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2025-08-03 16:31:29 +0200

    refactor: delete the console.log statements

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function getComputerChoice() {
     const rock = document.querySelector('#computer-rock');
     const paper = document.querySelector('#computer-paper');
     const scissors = document.querySelector('#computer-scissors');
+
     // Get random number between 1 and 30
     let random = Math.floor(Math.random() * 30) + 1;
     if(random <= 10) {
@@ -16,7 +17,6 @@ function getComputerChoice() {
         scissors.setAttribute('style', 'background-color: rgb(101, 173, 119); border-width: 4px; transition: all 0.3s linear;')
         return 'scissors';
     }
-
 }
 
 
@@ -54,6 +54,10 @@ function playGame() {
     
 
     function playRound(humanChoice, computerChoice) {
+        if (humanScore >= 5 || computerScore >= 5) {
+            humanScoreCount.textContent = `You: `;
+            computerScoreCount.textContent = `Computer: `;
+        }
         if(humanChoice === computerChoice) {
             console.log('Draw!');
         }
@@ -62,18 +66,16 @@ function playGame() {
             || (humanChoice === 'rock' && computerChoice === 'scissors')
             || (humanChoice === 'scissors' && computerChoice === 'paper')) 
             {
-            console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
             humanScore++;
             humanScoreCount.textContent = `You: ${humanScore}`;
         }
         // If none of the statements above are true, then computer won
         else {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
             computerScore++;
             computerScoreCount.textContent = `Computer: ${computerScore}`;
         }
 
-        // Wait 1 seconds to change the color back to normal
+        // Wait 0.5 seconds to change the color back to normal
         setTimeout(function(){
             computerRock.setAttribute('style', 'background-color: rgba(110, 105, 82, 0.723); border: 3px black solid; transition: all 0.3s linear;');
             computerPaper.setAttribute('style', 'background-color: rgba(110, 105, 82, 0.723); border: 3px black solid; transition: all 0.3s linear;');
