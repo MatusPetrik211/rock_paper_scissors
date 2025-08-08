e46708eb508c2c711281e79307662534a4e902f3
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2025-08-08 22:56:04 +0200

    Delete unnecessary if statement that only resulted in console logging

--- script.js
+++ script.js
@@ -1,111 +1,120 @@
 function getComputerChoice() {
-    const rock = document.querySelector('#computer-rock');
-    const paper = document.querySelector('#computer-paper');
-    const scissors = document.querySelector('#computer-scissors');
+  const rock = document.querySelector("#computer-rock");
+  const paper = document.querySelector("#computer-paper");
+  const scissors = document.querySelector("#computer-scissors");
 
-    // Get random number between 1 and 30
-    let random = Math.floor(Math.random() * 30) + 1;
-    if(random <= 10) {
-        rock.setAttribute('style', 'background-color: rgb(101, 173, 119); border-width: 4px; transition: all 0.3s linear;')
-        return 'rock';
-    }
-    else if(random > 10 && random <= 20) {
-        paper.setAttribute('style', 'background-color: rgb(101, 173, 119); border-width: 4px; transition: all 0.3s linear;')
-        return 'paper';
-    }
-    else {
-        scissors.setAttribute('style', 'background-color: rgb(101, 173, 119); border-width: 4px; transition: all 0.3s linear;')
-        return 'scissors';
-    }
+  // Get random number between 1 and 30
+  let random = Math.floor(Math.random() * 30) + 1;
+  if (random <= 10) {
+    rock.setAttribute(
+      "style",
+      "background-color: rgb(101, 173, 119); border-width: 4px; transition: all 0.3s linear;"
+    );
+    return "rock";
+  } else if (random > 10 && random <= 20) {
+    paper.setAttribute(
+      "style",
+      "background-color: rgb(101, 173, 119); border-width: 4px; transition: all 0.3s linear;"
+    );
+    return "paper";
+  } else {
+    scissors.setAttribute(
+      "style",
+      "background-color: rgb(101, 173, 119); border-width: 4px; transition: all 0.3s linear;"
+    );
+    return "scissors";
+  }
 }
 
-
 function playGame() {
-    const result = document.querySelector('.result');
-    const humanScoreCount = document.querySelector('.player-score');
-    const computerScoreCount = document.querySelector('.computer-score');
-    const playerRock = document.querySelector('#player-rock');
-    const playerPaper = document.querySelector('#player-paper');
-    const playerScissors = document.querySelector('#player-scissors');
-    const computerRock = document.querySelector('#computer-rock');
-    const computerPaper = document.querySelector('#computer-paper');
-    const computerScissors = document.querySelector('#computer-scissors');
+  const result = document.querySelector(".result");
+  const humanScoreCount = document.querySelector(".player-score");
+  const computerScoreCount = document.querySelector(".computer-score");
+  const playerRock = document.querySelector("#player-rock");
+  const playerPaper = document.querySelector("#player-paper");
+  const playerScissors = document.querySelector("#player-scissors");
+  const computerRock = document.querySelector("#computer-rock");
+  const computerPaper = document.querySelector("#computer-paper");
+  const computerScissors = document.querySelector("#computer-scissors");
 
-    result.textContent = "";
+  result.textContent = "";
 
-    let humanScore = 0;
-    let computerScore = 0;
+  let humanScore = 0;
+  let computerScore = 0;
 
-    humanScoreCount.textContent = `You: ${humanScore}`;
-    computerScoreCount.textContent = `Computer: ${computerScore}`;
+  humanScoreCount.textContent = `You: ${humanScore}`;
+  computerScoreCount.textContent = `Computer: ${computerScore}`;
 
-    playerRock.addEventListener('click', () => {
-        let computerSelection = getComputerChoice(); 
-        playRound('rock', computerSelection);
-    });
-    playerPaper.addEventListener('click', () => {
-        let computerSelection = getComputerChoice();
-        playRound('paper', computerSelection);
-    });
-    playerScissors.addEventListener('click', () => {
-        let computerSelection = getComputerChoice();
-        playRound('scissors', computerSelection);
-    });
-    
+  playerRock.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    playRound("rock", computerSelection);
+  });
+  playerPaper.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    playRound("paper", computerSelection);
+  });
+  playerScissors.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    playRound("scissors", computerSelection);
+  });
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanScore >= 5 || computerScore >= 5) {
-            humanScoreCount.textContent = `You: `;
-            computerScoreCount.textContent = `Computer: `;
-        }
-        if(humanChoice === computerChoice) {
-            console.log('Draw!');
-        }
-        // Checking all the possible choices when player would've won
-        else if ((humanChoice === 'paper' && computerChoice === 'rock')
-            || (humanChoice === 'rock' && computerChoice === 'scissors')
-            || (humanChoice === 'scissors' && computerChoice === 'paper')) 
-            {
-            humanScore++;
-            humanScoreCount.textContent = `You: ${humanScore}`;
-        }
-        // If none of the statements above are true, then computer won
-        else {
-            computerScore++;
-            computerScoreCount.textContent = `Computer: ${computerScore}`;
-        }
-
-        // Wait 0.5 seconds to change the color back to normal
-        setTimeout(function(){
-            computerRock.setAttribute('style', 'background-color: rgba(110, 105, 82, 0.723); border: 3px black solid; transition: all 0.3s linear;');
-            computerPaper.setAttribute('style', 'background-color: rgba(110, 105, 82, 0.723); border: 3px black solid; transition: all 0.3s linear;');
-            computerScissors.setAttribute('style', 'background-color: rgba(110, 105, 82, 0.723); border: 3px black solid; transition: all 0.3s linear;');
-        }, 500);
-
-        if(humanScore ===  5 || computerScore === 5) {
-            showEndMessage();
-        }
+  function playRound(humanChoice, computerChoice) {
+    if (humanScore >= 5 || computerScore >= 5) {
+      humanScoreCount.textContent = `You: `;
+      computerScoreCount.textContent = `Computer: `;
+    }
+    // Checking all the possible choices when player would've won
+    if (
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      humanScore++;
+      humanScoreCount.textContent = `You: ${humanScore}`;
+    }
+    // If none of the statements above are true, then computer won
+    else {
+      computerScore++;
+      computerScoreCount.textContent = `Computer: ${computerScore}`;
     }
 
+    // Wait 0.5 seconds to change the color back to normal
+    setTimeout(function () {
+      computerRock.setAttribute(
+        "style",
+        "background-color: rgba(110, 105, 82, 0.723); border: 3px black solid; transition: all 0.3s linear;"
+      );
+      computerPaper.setAttribute(
+        "style",
+        "background-color: rgba(110, 105, 82, 0.723); border: 3px black solid; transition: all 0.3s linear;"
+      );
+      computerScissors.setAttribute(
+        "style",
+        "background-color: rgba(110, 105, 82, 0.723); border: 3px black solid; transition: all 0.3s linear;"
+      );
+    }, 500);
 
-    function showEndMessage() {
-        if(humanScore > computerScore) {
-            result.textContent = "You've won. CONGRATULATIONS!!";
-        }
-        else if(computerScore > humanScore) {
-            result.textContent = "You've lost. Try again next time!!";
-        }
-
-        // Wait before resetting the score
-        setTimeout(function(){
-            humanScore = 0;
-            computerScore = 0;
-            humanScoreCount.textContent = `You: ${humanScore}`;
-            computerScoreCount.textContent = `Computer: ${computerScore}`;
-            result.textContent = "";
-        }, 1000);
-        
+    if (humanScore === 5 || computerScore === 5) {
+      showEndMessage();
     }
+  }
+
+  function showEndMessage() {
+    if (humanScore > computerScore) {
+      result.textContent = "You've won. CONGRATULATIONS!!";
+    } else if (computerScore > humanScore) {
+      result.textContent = "You've lost. Try again next time!!";
+    }
+
+    // Wait before resetting the score
+    setTimeout(function () {
+      humanScore = 0;
+      computerScore = 0;
+      humanScoreCount.textContent = `You: ${humanScore}`;
+      computerScoreCount.textContent = `Computer: ${computerScore}`;
+      result.textContent = "";
+    }, 1000);
+  }
 }
 
 playGame();
